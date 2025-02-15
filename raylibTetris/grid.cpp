aef495673994395194137f86f4e0af2080f25500
Augusto Braz Chevitarese <146674217+augusto1109@users.noreply.github.com>
2025-02-15 08:48:40 -0300

    Update grid.cpp

--- raylibTetris/grid.cpp
+++ raylibTetris/grid.cpp
@@ -4,4 +4,13 @@ Grid::Grid() {
   numRows = 20;
   numCols = 10;
   cellSize = 30;
+  Initialize();
+}
+
+void Grid::Initialize() {
+  for(int row = 0; row < numRows; row++) {
+    for(int column = 0; column < numCols; column++) {
+      grid[row][column] = 0;
+    }
+  }
 }
