a30029a3868cffef43ce04dbc76419dc1c364b41
Augusto Braz Chevitarese <146674217+augusto1109@users.noreply.github.com>
2025-02-15 08:54:50 -0300

    Update main.cpp

--- raylibTetris/main.cpp
+++ raylibTetris/main.cpp
@@ -1,10 +1,14 @@
 #include <raylib.h>
+#include "grid.h"
 
 int main() {
   Color darkBlue = {44, 44, 127, 255};
   InitWindow(300, 600, "raylib Tetris");
   SetTargetFPS(60);
 
+  Grid grid = Grid();
+  grid.Print();
+  
   while(WindowShouldClose() == false) {
     BeginDrawing();
     ClearBackground(darkBlue);
